2981cb207be103177ae3a8af2e03c03493893849
Mohamad D Abdulkarim <mohamaddabdulkarim@MacbookProDia.local>
2019-09-17 22:31:13 -0400

    added the image features for each blog post

--- ottoman/theme/admin.py
+++ ottoman/theme/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+# In myapp/admin.py
+
+from copy import deepcopy
+from django.contrib import admin
+from mezzanine.blog.admin import BlogPostAdmin
+from mezzanine.blog.models import BlogPost
+
+blog_fieldsets = deepcopy(BlogPostAdmin.fieldsets)
+blog_fieldsets[0][1]["fields"].insert(-2, "retina")
+
+class MyBlogPostAdmin(BlogPostAdmin):
+    fieldsets = blog_fieldsets
+
+admin.site.unregister(BlogPost)
+admin.site.register(BlogPost, MyBlogPostAdmin)
